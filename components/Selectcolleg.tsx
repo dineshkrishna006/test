1ebd77ddadda230d28348b4091617ad8269c00c1
dinesh-p0 <dineshkrishna0456@gmail.com>
2024-06-08 13:40:18 +0530

    a

--- components/Selectcolleg.tsx
+++ components/Selectcolleg.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
-import colleges from "@/lib/utils"
+import * as React from "react";
+import colleges from "@/lib/utils";
 import {
   Select,
   SelectContent,
@@ -7,27 +7,30 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
 export function SelectCollege() {
   return (
-    <Select >
-      <SelectTrigger  className="outline-[#3D408A] focus:outline-[#3D408A]" >
-        <SelectValue placeholder="Select College"  className="focus:outline-[#3D408A] outline-[#3D408A]" />
+    <Select>
+      <SelectTrigger className="outline-[#3D408A] focus:outline-[#3D408A]">
+        <SelectValue
+          placeholder="Select College"
+          className="focus:outline-[#3D408A] outline-[#3D408A]"
+        />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup >
-            {
-                colleges.map(clg=>(
-                    <SelectItem  key={clg.id} value={clg.name} className="focus:outline-none" >{clg.name}</SelectItem>
-                ))
-            }
-          {/* <SelectItem value="mlrit">Eastern Standard Time (EST)</SelectItem> */}
-          {/* <SelectItem value="cmr">Central Standard Time (CST)</SelectItem>
-          <SelectItem value="mlritm">Mountain Standard Time (MST)</SelectItem>
-          <SelectItem value="bvrit">Pacific Standard Time (PST)</SelectItem> */}
+        <SelectGroup>
+          {colleges.map((clg) => (
+            <SelectItem
+              key={clg.id}
+              value={clg.name}
+              className="focus:outline-none"
+            >
+              {clg.name}
+            </SelectItem>
+          ))}
         </SelectGroup>
       </SelectContent>
     </Select>
-  )
+  );
 }
